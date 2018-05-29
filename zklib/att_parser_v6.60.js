a999961f4f8feb2a0e2bfac1f8672366d1238c78
Matias Ribichich <guli4073@gmail.com>
2018-05-29 16:47:49 -0300

    Fixed large uid attendance parsing

--- zklib/att_parser_v6.60.js
+++ zklib/att_parser_v6.60.js
@@ -2,9 +2,12 @@ const timeParser = require('./timestamp_parser');
 
 module.exports.name = 'v6.60';
 
+const uidIndex = 4;
+const uidLength = 9;
+
 module.exports.parse = attdata => ({
   id: (attdata[3] << 8) + attdata[2],
-  uid: parseInt(attdata.slice(4, 6).toString('ascii')),
+  uid: parseInt(attdata.slice(uidIndex, uidIndex + uidLength).toString('ascii')),
   state: attdata[28],
   timestamp: timeParser.decode(attdata.readUInt32LE(29))
 });
